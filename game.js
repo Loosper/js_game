9f5aa61e59550f5e780eca56d4630dad157d9fec
root <vitonaychev@gmail.com>
2018-01-23 17:36:42 +0200

    Obstacles generation every one second and faster scroll when top hit

--- game.js
+++ game.js
@@ -8,7 +8,8 @@ window.onload = function(_) {
         }
     }
 
-    var SCROLL_SPEED = 1;
+    var DEFAULT_SCROLL_SPEED = 1;
+    var SCROLL_SPEED = DEFAULT_SCROLL_SPEED;
     var GRAVITY = 2;
     var canvas = document.getElementById("canvas");
     var ctx;
@@ -35,9 +36,7 @@ window.onload = function(_) {
         },
 
         change_y: function(step) {
-            if (this.y + step < 0)
-                this.y = 0;
-            else if (this.y + this.height + step > canvas.height)
+            if (this.y + this.height + step > canvas.height)
                 this.y = canvas.height - this.height;
             else
                 this.y += step;
@@ -48,60 +47,77 @@ window.onload = function(_) {
     var leftPressed = false;
     var rightPressed = false;
     var jump_frames = 0;
+    var player_stable = true;
 
     document.addEventListener("keydown", function(ev){
         console.log(ev.key);
         if (ev.key == 'ArrowLeft') leftPressed = true;
-        else if (ev.key == 'ArrowRight') rightPressed = true;
-        else if (ev.key == ' ') jump_frames = 20;
+        if (ev.key == 'ArrowRight') rightPressed = true;
+        if (ev.key == ' ' && (player_stable  || player.y == canvas.height - player.height)) jump_frames = 25;
     });
 
     document.addEventListener("keyup", function(ev){
-        if (ev.key == 'ArrowLeft') leftPressed = false;
-        else if (ev.key == 'ArrowRight') rightPressed = false;
+        if(ev.key == 'ArrowLeft') leftPressed = false;
+        if(ev.key == 'ArrowRight')rightPressed = false;
         // else if (ev.key == ' ') spacePressed = false;
     });
 
     function make_obstacle(y) {
         return new Obstacle(Math.floor(Math.random() * canvas.width), y);
     }
-
-    function draw() {
+    
+    function is_player_stable(player, obstacles){
         var player_stable = false;
-
+        
         for (platform of obstacles) {
-            platform.y += SCROLL_SPEED;
-
             if (player.standing(platform)) {
                 player_stable = true;
             }
+        }
+        return player_stable;
+    }
+
+    function update() {
+        for (platform of obstacles) {
+            platform.y += SCROLL_SPEED;
+        }
+        player_stable = is_player_stable(player, obstacles);
+
+        if(player.y < 0){
+            SCROLL_SPEED = 3;
+        }else{
+            SCROLL_SPEED = DEFAULT_SCROLL_SPEED;
         }
 
         var top = obstacles[obstacles.length - 1];
         if (top.y + top.height > canvas.height) {
             obstacles.pop();
-            obstacles.unshift(make_obstacle(0));
         }
 
         if (leftPressed) player.change_x(-5);
         if (rightPressed) player.change_x(5);
-        if (jump_frames-- > 0) player.change_y(-4);
+        if (jump_frames-- > 0) player.change_y(-jump_frames);
 
         if (!player_stable)
-            player.change_y(GRAVITY);
+            player.change_y(GRAVITY + SCROLL_SPEED - DEFAULT_SCROLL_SPEED);
+        
+        window.setTimeout(update, 10);
+    }
+
+    function draw() {
 
         // render
         ctx.fillStyle = "#FFFFFF";
         ctx.globalAlpha = 0.1;
         ctx.fillRect(0, 0, canvas.width, canvas.height, 0.5);
         ctx.globalAlpha = 1;
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(player.x, player.y, player.width, player.height);
         ctx.fillStyle = "#0000FF";
 
         for (element of obstacles) {
             ctx.fillRect(element.x, element.y, element.width, element.height);
         }
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(player.x, player.y, player.width, player.height);
 
         window.requestAnimationFrame(draw);
     }
@@ -113,9 +129,16 @@ window.onload = function(_) {
         return;
     }
 
-    for (var i = 0; i < canvas.height; i += 50) {
+    function generate_enemy(){
+       obstacles.unshift(make_obstacle(0));
+        window.setTimeout(generate_enemy, 1000);
+    }
+
+    for (var i = 0; i < canvas.height; i += 100) {
         obstacles.push(make_obstacle(i));
     }
 
+    update();
+    generate_enemy();
     draw();
 }
