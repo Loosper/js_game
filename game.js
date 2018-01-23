8e03f2944c4e4476b45bbda0c48950db2aae1b15
Loosper <boian4o1@gmail.com>
2018-01-23 15:02:37 +0200

    Cleanup

--- game.js
+++ game.js
@@ -8,51 +8,55 @@ window.onload = function(_) {
         }
     }
 
-    var canvas = document.getElementById("tutorial");
-
+    var canvas = document.getElementById("canvas");
+    var ctx;
     var player = {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
         width: 50,
-        height: 50
+        height: 50,
+        x: canvas.width / 2,
+        y: canvas.height - 50
     }
 
     var gravity = 1;
-
     var obstacles = [];
-    for (var i = 0; i < 6; i++) {
-        obstacles.push(new Obstacle(10 * i, 20 * i));
-    }
     var leftPressed = false;
     var rightPressed = false;
+    var jump_frames = 0;
+
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+    } else {
+        alert("this browser is bad");
+        return;
+    }
+
+    for (var i = 0; i < canvas.height; i += 50) {
+        obstacles.push(
+            new Obstacle(Math.floor(Math.random() * canvas.width), i)
+        );
+    }
 
     document.addEventListener("keydown", function(ev){
-        console.log("down");
-        var key = ev.keyCode;
-            if(key == 37) leftPressed = true;
-            if (key == 39) rightPressed = true;
-            //if(key == 39) player.x += 5;
-            //if(key == 39) player.x += 5;
-
+        console.log(ev.key);
+        if (ev.key == 'ArrowLeft') leftPressed = true;
+        else if (ev.key == 'ArrowRight') rightPressed = true;
+        else if (ev.key == ' ') jump_frames = 20;
     });
 
     document.addEventListener("keyup", function(ev){
-        console.log("up");
-        var key = ev.ketCode;
-        rightPressed = false;
-        leftPressed = false;
+        if (ev.key == 'ArrowLeft') leftPressed = false;
+        else if (ev.key == 'ArrowRight') rightPressed = false;
+        // else if (ev.key == ' ') spacePressed = false;
     });
 
     function draw() {
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
-        } else {
-            alert("this browser is bad");
-            return;
-        }
-
         if (leftPressed) player.x -= 5;
         if (rightPressed) player.x += 5;
+        if (jump_frames-- > 0) player.y -= 4;
+
+        // for (platform of obstacles) {
+        //
+        // }
 
         ctx.fillStyle = "#FFFFFF";
         ctx.globalAlpha = 0.1;
@@ -62,7 +66,7 @@ window.onload = function(_) {
         ctx.fillRect(player.x, player.y, player.width, player.height);
         ctx.fillStyle = "#0000FF";
 
-        for (element in obstacles) {
+        for (element of obstacles) {
             ctx.fillRect(element.x, element.y, element.width, element.height);
         }
 
